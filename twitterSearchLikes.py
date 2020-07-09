fb018b1a048557d4cc36f44aab7bdce1e5f6b6ce
Kevin Tran <ktran1997@gmail.com>
2020-07-09 16:42:38 -0700

    All files

---
+++ twitterSearchLikes.py
@@ -0,0 +1,37 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("API Key ", 
+	"API Secret Key")
+auth.set_access_token("Access Token", 
+	"Access Token Secret")
+
+# Create API object
+api = tweepy.API(auth)
+
+saveFile = open('getLikedTweets.txt', 'a')
+saveFile.truncate(0)
+
+for favorite in tweepy.Cursor(api.favorites, id="Kev1n_Trxn").items(20):
+	saveFile.write("%s %s %s %s \n"%(
+		'Screen Name: '+str(favorite.user.screen_name)+',', 
+		'Name: '+str(favorite.user.name)+',', 
+		'Tweet Id: https://twitter.com/user/status/'+str(favorite.id)+',',
+		'Tweet Text: '+str(favorite.text)
+		))
+
+saveFile.close()
+
+searchFile = open('getLikedTweets.txt', 'r')
+print("Hello, Which tweet would you like to find? ")
+userInput = input()
+
+for line in searchFile:
+    if userInput in line: 
+    	line.replace(',','\n')
+    	print(line)
+
+searchFile.close()
+
+
+
